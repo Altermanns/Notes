1190c99fdf0468ebf77bee96f5a5f77a7e95269c
Altermanns <isaac.277.trujillo@gmail.com>
2024-12-16 16:43:41 -0500

    Creación del modelo de vista Acerca de MVVM

--- Notes/Views/AboutPage.xaml.cs
+++ Notes/Views/AboutPage.xaml.cs
@@ -6,18 +6,4 @@ public partial class AboutPage : ContentPage
     {
         InitializeComponent();
     }
-
-    private async void OpenLink_Tapped(object sender, EventArgs e)
-    {
-        string url = "https://github.com/Altermanns/Notes.git";
-        try
-        {
-            await Launcher.Default.OpenAsync(url);
-        }
-        catch (Exception ex)
-        {
-            // Manejo de errores si no se puede abrir el enlace
-            await DisplayAlert("Error", $"No se pudo abrir el enlace: {ex.Message}", "OK");
-        }
-    }
 }
